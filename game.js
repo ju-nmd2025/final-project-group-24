a790984e51a70a56788cbee95f129473950d867a
ximemendoza <mexi24tp@student.ju.se>
2025-12-11 11:08:49 +0100

    Game creation part 1
    Create the canvas, give color to the background, and insert the character with the right size

--- game.js
+++ game.js
@@ -1,42 +1,28 @@
-import { character } from "./character";
-import platform from "platform";
+import { Character } from "./character";
+import { Platform } from "./plataform";
+
+let gravity = 0.35;
+let jumpForce = -10.5;
+let platforms = [];
 
 function setup() {
-    createCanvas(400, 400);
+  createCanvas(400, 600);
 }
 
-// Obstacle / Spike / Death
-function drawObstacle() {
-    push();
-    fill("red");
-    triangle(180, 300, 210, 240, 240, 300);
-    pop();
-}
-
-let x = 100;
-let y = 100;
+character = new Character( //to indicate what parameter we want for the size of the character
+  width / 2 - 40 / 2, // x
+  height - 100, // y
+  40, // w
+  40, // h
+  jumpForce, // vy
+  4 // speed
+);
 
 function draw() {
-    background(100, 100, 100);
-
-    character.draw();
-	platform.draw();
-
-    platform.x -= 10;
-    if(platform.x + platform.w < 0){
-        platform.x = 500;
-    }
-
-    if(character.y + character.h < 300){
-        character.y += 10;
-    }
-
-    // Floor
-    line(0, 300, 400, 300);
+  background(155, 231, 255);
+  drawcharacter();
 }
 
-function keyPressed(){
-    if(character.y + character.h === 300){
-        character.y -= 80;
-    }
+function drawcharacter() {
+  character.draw(); //indicates to draw the character with the form created
 }
