72fddc37feb79976695ee87735a8b87801fed037
Quynh <vonhuquynhtran99@gmail.com>
2025-12-11 11:36:53 +0100

    add breaking platform
    I just add the breaking platform

--- platform.js
+++ platform.js
@@ -1,15 +1,63 @@
-export default class Platform {
-  constructor(x, y, w, h) {
+export class Platform {
+  constructor(x, y, w, h, type = "normal") {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
+
+    this.type = type;
+    this.broken = false;
+
+    //animation break
+    this.breakProgress = 0;
+  }
+
+  update() {
+    if (this.broken) {
+      this.breakProgress += 0.08;
+      if (this.breakProgress > 1) this.breakProgress = 1;
+      this.y += 5;
+    }
   }
 
   draw() {
+    // draw platform
+    if (!this.broken) {
+      push();
+      if (this.type === "breakable") {
+        fill(160, 82, 45); // breakable platform
+      } else {
+        fill(46, 125, 50); // normal platform
+      }
+      noStroke();
+      rect(this.x, this.y, this.w, this.h, 4);
+      pop();
+      return;
+    }
+
+    // break platform
+    const t = this.breakProgress;
+    const gap = 4 + t * 12;
+    const angle = t * 0.4;
+
+    const halfW = this.w / 2;
+
+    // left
     push();
-    fill(100, 200, 100);
-    rect(this.x, this.y, this.w, this.h);
+    translate(this.x + halfW / 2 - gap, this.y);
+    rotate(-angle);
+    fill(160, 82, 45);
+    noStroke();
+    rect(-halfW / 2, 0, halfW, this.h, 4);
+    pop();
+
+    // right
+    push();
+    translate(this.x + halfW + halfW / 2 + gap, this.y);
+    rotate(angle);
+    fill(160, 82, 45);
+    noStroke();
+    rect(-halfW / 2, 0, halfW, this.h, 4);
     pop();
   }
 }
